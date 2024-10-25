d42834c8a410f26854467f24af101717e961feb1
IOMART <129962252+booccp@users.noreply.github.com>
2024-10-25 10:33:55 +0800

    Update disable-ipv6.sh

--- disable-ipv6.sh
+++ disable-ipv6.sh
@@ -16,6 +16,18 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
+# 检查是否已禁用IPv6
+if sysctl net.ipv6.conf.all.disable_ipv6 | grep -q "1"; then
+  green_echo "IPv6已禁用。"
+  exit 0
+fi
+
+# 检查是否包含IPv6
+if ! ip a | grep -q "inet6"; then
+  green_echo "本机不包含IPv6地址。"
+  exit 0
+fi
+
 # 禁用IPv6
 green_echo "禁用IPv6..."
 
@@ -33,6 +45,10 @@ sed -i '/net.ipv6.conf.eth0.disable_ipv6/d' /etc/sysctl.conf
 } >> /etc/sysctl.conf
 
 # 重新加载sysctl配置
-sysctl -p
+sysctl -p &> /dev/null
+if [ $? -ne 0 ]; then
+  red_echo "重新加载sysctl配置失败。"
+  exit 1
+fi
 
 green_echo "IPv6已禁用。"
